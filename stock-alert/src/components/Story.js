add4fffd14d7c6d4b10fdd3ad40393a29c02ffd4
Aduno <67022316+Aduno@users.noreply.github.com>
2022-06-23 21:53:08 -0400

    completed stories section and page

--- stock-alert/src/components/Story.js
+++ stock-alert/src/components/Story.js
@@ -5,9 +5,11 @@ export default function Story(props) {
   return (
     // Title
     <div className='container-fluid story'>
-      <h1 className='story-title'>{props.title}</h1>
-      <p className='date'>{props.date}</p>
-      <p className='author'>{props.author}</p>
+      <div className='header'>
+        <h1 className='story-title'>{props.title}</h1>
+        <p className='date'>Date: {props.date}</p>
+        <p className='author'>Author: {props.author}</p>
+      </div>
       <div className='img-container'>
         <img src={require('../img/'+props.src)} className='main-img'/>
       </div>
